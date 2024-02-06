8ae7ebb03a7230fabc4c802abb69e4b7cbb112a7
UchihaShaco <ja3far54@hotmail.com>
2024-02-06 18:51:51 +0400

    cpp05 ex00,01

---
+++ cpp05/ex02/Bureaucrat.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include "Form.hpp"
+#include <iostream>
+#include <stdexcept>
+class Form;
+class Bureaucrat
+{
+private:
+    const std::string name;
+    int grade;
+
+public:
+    class GradeTooHighException : public std::exception
+    {
+    public:
+        virtual const char *what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception
+    {
+    public:
+        virtual const char *what() const throw();
+    };
+
+    Bureaucrat(const std::string &name, int grade);
+    ~Bureaucrat();
+
+    const std::string &getName() const;
+    int getGrade() const;
+    void signForm(Form &form);
+    void incrementGrade();
+    void decrementGrade();
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
+
+// to copy everything from current directory to ../ex02 use the following command in the terminal: cp * ../ex02
